2b297ee51f779a5dc3e82ffafa05587ce7580778
ClebersonCosta <cleberson-costa@outlook.com>
2025-05-08 14:51:23 -0300

    test: add annotation to improve test execution

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -1,16 +1,18 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
 
     @Test
-    void deveRetornarNumerosPares() {
+    void deve_retornar_numeros_pares_() {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosEsperados = Arrays.asList(2, 4);
         List<Integer> resultado = FiltroNumeros.numerosPares(lista);
@@ -18,7 +20,7 @@ class FiltroNumerosTest {
     }
 
     @Test
-    void deveRetornarNumerosImpares() {
+    void deve_Retornar_Numeros_Impares_() {
         List<Integer> lista = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosEsperados = Arrays.asList(1, 3);
         List<Integer> resultado = FiltroNumeros.numerosImpares(lista);
@@ -26,7 +28,7 @@ class FiltroNumerosTest {
     }
 
     @Test
-    void deveAceitarSomenteNumeroPositivo() {
+    void deve_Aceitar_Somente_Numero_Positivo_() {
         boolean positivo = FiltroNumeros.isPositivo(1);
         assertTrue(positivo);
     }
